22e59286faea645e81d683f38b4313e3de2d0740
Marco Heisig <marco.heisig@fau.de>
2016-11-14 12:23:09 +0100

    finished strided array transform and added many test cases for it

--- code/strided-array/transform.lisp
+++ code/strided-array/transform.lisp
@@ -4,17 +4,24 @@
 
 (defmethod transform ((object strided-array-index-space)
                       (transformation affine-transformation))
-  (let ((ranges (make-array (dimension object))))
+  (let ((result (make-array (output-dimension transformation)))
+        (ranges (ranges object)))
+    (loop for input-constraint across (input-constraints transformation)
+          and range across ranges do
+          (when input-constraint
+            (assert (and (unary-range? range)
+                         (= (range-start range)
+                            input-constraint)))))
     (loop for p across (permutation transformation)
           and i from 0 do
-            (setf (aref ranges i)
+            (setf (aref result i)
                   (let ((a (aref (affine-coefficients transformation) i 0))
                         (b (aref (affine-coefficients transformation) i 1))
-                        (x (or (aref (ranges object) p)
+                        (x (or (and p (aref ranges p))
                                (range 0 1 0))))
                     (range (+ (* a (range-start x)) b)
                            (* (range-step x) a)
                            (+ (* (range-end x) a) b)))))
     (make-instance
      'strided-array-index-space
-     :ranges ranges)))
+     :ranges result)))
