4647042eda03b086b7c1642b12c26de51a2349b0
Marco Heisig <marco.heisig@fau.de>
2017-09-03 19:55:19 +0200

    vastly improved range difference computation

--- code/data-structures/range.lisp
+++ code/data-structures/range.lisp
@@ -1,5 +1,4 @@
 ;;; © 2016-2017 Marco Heisig - licensed under AGPLv3, see the file COPYING
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (in-package :petalisp)
 
@@ -33,29 +32,54 @@
 (defmethod difference ((space-1 range) (space-2 range))
   ;; we only care about the part of space-2 that intersects with space-1
   (let ((space-2 (intersection space-1 space-2)))
-    (if (not space-2) `(,space-1)
+    (if (not space-2)
+        (list space-1)
         (let ((start-1 (range-start space-1))
               (step-1 (range-step space-1))
               (end-1 (range-end space-1))
               (start-2 (range-start space-2))
               (step-2 (range-step space-2))
-              (end-2 (range-end space-2)))
-          ;; There are two options to form the difference of two
-          ;; ranges. Either by creating ranges with the step size of
-          ;; space-1, or by creating ranges with the step size of
-          ;; space-2. Which one is more efficient depends on the situation.
-          (flet ((range (start step end)
+              (end-2 (range-end space-2))
+              result)
+          (flet ((maybe-push-range (start step end)
                    (when (<= start-1 start end end-1)
-                     (range start step end))))
-            (loop for x from start-2 below end-2 by step-2
-                  when (range (+ x step-1) step-1 (- (+ x step-2) step-1))
-                    collect it into result
-                  finally
-                     (if-let (it (range start-1 step-1 (- start-2 step-1)))
-                       (push it result))
-                     (if-let (it (range (+ end-2 step-1) step-1 end-1))
-                       (push it result))
-                     (return result)))))))
+                     (push (range start step end) result))))
+            (if (and (/= step-2 1)
+                     (<= (the positive-integer (/ step-2 step-1))
+                         (size space-2)))
+                ;; Case 1: ranges with step size step-2
+                (let ((include-lowers (> start-1 (- start-2 step-2)))
+                      (include-uppers (< end-1 (+ end-2 step-2))))
+                  (unless include-lowers
+                    (maybe-push-range start-1 step-1 (- start-2 step-1)))
+                  (unless include-uppers
+                    (maybe-push-range (+ end-2 step-1) step-1 end-1))
+                  (iterate (for offset from step-1 by step-1 below step-2)
+                           (for start = (if include-lowers
+                                            (+ start-2 (- step-2) offset)
+                                            (+ start-2 offset)))
+                           (for end = (if include-uppers
+                                          (+ end-2 offset)
+                                          (+ end-2 (- step-2) offset)))
+                           (maybe-push-range
+                            (if (< start start-1) (+ start step-2) start)
+                            step-2
+                            (if (< end-1 end) (- end step-2) end))))
+                ;; Case 2: ranges with step-size step-1
+                (let ((lower-max (- start-2 step-1))
+                      (upper-min (+ end-2 step-1)))
+                  ;; process the boundaries
+                  (cond ((and (= lower-max start-1) (= upper-min end-1))
+                         (maybe-push-range start-1 (- end-1 start-1) end-1))
+                        (t
+                         (maybe-push-range start-1 step-1 lower-max)
+                         (maybe-push-range upper-min step-1 end-1)))
+                  (unless (= 1 step-2)
+                    ;; process the interior
+                    (iterate (for start from (+ start-2 step-1) below end-2 by step-2)
+                             (for end = (+ start step-2 (- step-1)))
+                             (maybe-push-range start step-1 end))))))
+          result))))
 
 (test |(difference range)|
   (flet ((? (a b)
@@ -65,6 +89,7 @@
                                     (difference a b))))
                (is (equal? a (first (difference a b)))))))
     (? (range 1 3 7) (range 8 1 22))
+    (? (range 1 1 5) (range 6 1 10))
     (? (range 1 3 7) (range 1 3 10))
     (? (range 1 3 13) (range 4 2 10))
     (? (range 0 1 9) (range 2 2 4))
