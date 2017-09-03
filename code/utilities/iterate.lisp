3ba0a787e07a2cc65c65257d988feb39cd3de36a
Marco Heisig <marco.heisig@fau.de>
2017-09-03 12:54:19 +0200

    simple iterate wrapper to always emit type declarations

---
+++ code/utilities/iterate.lisp
@@ -0,0 +1,11 @@
+;;; © 2016-2017 Marco Heisig - licensed under AGPLv3, see the file COPYING
+
+(in-package :petalisp)
+
+(defmacro iterate (&body body)
+  "ITERATE with variable declarations"
+  `(iterate:iterate
+     ,@(if (symbolp (first body))
+           (list* (first body)
+                  '(declare (iterate:declare-variables)) (rest body))
+           (list* '(declare (iterate:declare-variables)) body))))
