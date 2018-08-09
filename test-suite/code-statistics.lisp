00e78d2b85de64ec49658bdede5e7d378133e128
Marco Heisig <marco.heisig@fau.de>
2018-08-09 09:11:42 +0200

    Improve the code statistics printed when the test suite is run.

--- test-suite/code-statistics.lisp
+++ test-suite/code-statistics.lisp
@@ -31,15 +31,44 @@
        (format stream "The system ~s consists of ~d lines of code in ~d file~:P.~%"
                (asdf:primary-system-name system) lines-of-code files)))
 
+(defun class-name-p (symbol)
+  (and (find-class symbol nil) t))
+
+(defun macro-name-p (symbol)
+  (and (macro-function symbol) t))
+
+(defun defun-name-p (symbol)
+  (and (fboundp symbol)
+       (not (typep (symbol-function symbol) 'generic-function))))
+
+(defun defgeneric-name-p (symbol)
+  (and (fboundp symbol)
+       (typep (symbol-function symbol) 'generic-function)))
+
+(defun symbol-methods (symbol)
+  (if (not (fboundp symbol))
+      '()
+      (let ((f (symbol-function symbol)))
+        (if (typep f 'generic-function)
+            (generic-function-methods f)
+            '()))))
+
 (defun print-package-statistics (package &optional (stream *standard-output*))
-  (loop
-    for sym being the present-symbols of package
-    counting (find-class sym nil) into classes
-    counting (macro-function sym) into macros
-    counting (fboundp sym) into functions
-    finally
-       (format stream "The package ~s defines ~d functions, ~d macros and ~d classes.~%"
-               (package-name (find-package package)) functions macros classes)))
+  (loop for symbol being the present-symbols of package
+        counting (class-name-p symbol) into classes
+        counting (macro-name-p symbol) into macros
+        counting (defun-name-p symbol) into defuns
+        counting (defgeneric-name-p symbol) into defgenerics
+        summing (length (symbol-methods symbol)) into methods
+        finally
+           (format
+            stream
+            "~<The package ~S defines ~D classes, ~D macros and ~D functions.  ~
+               ~D of the functions are generic.  ~
+               Each generic function has, on average, ~,2F methods.~@:>~%"
+            (list
+             (package-name (find-package package))
+             classes macros (+ defuns defgenerics) defgenerics (float (/ methods defgenerics))))))
 
 (defun print-platform-information (&optional (stream *standard-output*))
   (format stream "Implementation: ~:[Something weird~;~:*~a~]"
