4601c4b3a7d77d68d0c103221fee0758d0cf5e73
Marco Heisig <marco.heisig@fau.de>
2019-06-21 08:05:03 +0200

    Finish the rewrite rules of +, -, * and /.

--- code/specialization/common-lisp/numbers.lisp
+++ code/specialization/common-lisp/numbers.lisp
@@ -288,242 +288,352 @@
 ;;;
 ;;; Addition
 
-(defop (+ integer-+) (integer) (integer integer))
-(defop (+ rational-+) (rational) (rational rational))
-(defop (+ short-float-+) (short-float) (short-float short-float))
-(defop (+ single-float-+) (single-float) (single-float single-float))
-(defop (+ double-float-+) (double-float) (double-float double-float))
-(defop (+ long-float-+) (long-float) (long-float long-float))
-(defop (+ complex-short-float-+) (complex-short-float) (complex-short-float complex-short-float))
-(defop (+ complex-single-float-+) (complex-single-float) (complex-single-float complex-single-float))
-(defop (+ complex-double-float-+) (complex-double-float) (complex-double-float complex-double-float))
-(defop (+ complex-long-float-+) (complex-long-float) (complex-long-float complex-long-float))
+(defop (+ add-integers) (integer) (integer integer))
+(defop (+ add-rationals) (rational) (rational rational))
+(defop (+ add-short-floats) (short-float) (short-float short-float))
+(defop (+ add-single-floats) (single-float) (single-float single-float))
+(defop (+ add-double-floats) (double-float) (double-float double-float))
+(defop (+ add-long-floats) (long-float) (long-float long-float))
+(defop (+ add-complex-short-floats) (complex-short-float) (complex-short-float complex-short-float))
+(defop (+ add-complex-single-floats) (complex-single-float) (complex-single-float complex-single-float))
+(defop (+ add-complex-double-floats) (complex-double-float) (complex-double-float complex-double-float))
+(defop (+ add-complex-long-floats) (complex-long-float) (complex-long-float complex-long-float))
 
-(defop (+ binary-+) (number) (number number) (a b)
+(defop (+ add) (number) (number number) (a b)
   (type-code-subtypecase (numeric-contagion a b)
     ((not number) (abort-specialization))
     (integer
      (rewrite-as
-      (integer-+
+      (add-integers
        (the-integer a)
        (the-integer b))))
     (rational
      (rewrite-as
-      (rational-+
+      (add-rationals
        (the-rational a)
        (the-rational b))))
     (short-float
      (rewrite-as
-      (short-float-+
+      (add-short-floats
        (coerce-to-short-float a)
        (coerce-to-short-float b))))
     (single-float
      (rewrite-as
-      (single-float-+
+      (add-single-floats
        (coerce-to-single-float a)
        (coerce-to-single-float b))))
     (double-float
      (rewrite-as
-      (double-float-+
+      (add-double-floats
        (coerce-to-double-float a)
        (coerce-to-double-float b))))
     (long-float
      (rewrite-as
-      (long-float-+
+      (add-long-floats
        (coerce-to-long-float a)
        (coerce-to-long-float b))))
     ((complex short-float)
      (rewrite-as
-      (complex-short-float-+
+      (add-complex-short-floats
        (coerce-to-complex-short-float a)
        (coerce-to-complex-short-float b))))
     ((complex single-float)
      (rewrite-as
-      (complex-single-float-+
+      (add-complex-single-floats
        (coerce-to-complex-single-float a)
        (coerce-to-complex-single-float b))))
     ((complex double-float)
      (rewrite-as
-      (complex-double-float-+
+      (add-complex-double-floats
        (coerce-to-complex-double-float a)
        (coerce-to-complex-double-float b))))
     ((complex long-float)
      (rewrite-as
-      (complex-long-float-+
+      (add-complex-long-floats
        (coerce-to-complex-long-float a)
        (coerce-to-complex-long-float b))))))
 
 (define-external-rewrite-rule + (&rest numbers)
   (if (null numbers)
       (rewrite-let () 0)
-      (labels ((rewrite-+ (number more-numbers)
-                 (if (null more-numbers)
-                     (rewrite-let ((number (process-argument number)))
-                       (rewrite-as (the-number number)))
-                     (rewrite-let ((a (process-argument number))
-                                   (b (rewrite-+ (first more-numbers) (rest more-numbers))))
-                       (rewrite-as (binary-+ a b))))))
-        (rewrite-+ (first numbers) (rest numbers)))))
+      (multiple-value-bind (type-codes value)
+          (rewrite-let ((number (process-argument (pop numbers))))
+            (rewrite-as (the-number number)))
+        (loop while numbers do
+          (multiple-value-setq (type-codes value)
+            (rewrite-let ((a (values type-codes value))
+                          (b (process-argument (pop numbers))))
+              (rewrite-as (add a b)))))
+        (values type-codes value))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Subtraction
 
-(defop (- integer--) (integer) (integer integer))
-(defop (- rational--) (rational) (rational rational))
-(defop (- short-float--) (short-float) (short-float short-float))
-(defop (- single-float--) (single-float) (single-float single-float))
-(defop (- double-float--) (double-float) (double-float double-float))
-(defop (- long-float--) (long-float) (long-float long-float))
-(defop (- complex-short-float--) (complex-short-float) (complex-short-float complex-short-float))
-(defop (- complex-single-float--) (complex-single-float) (complex-single-float complex-single-float))
-(defop (- complex-double-float--) (complex-double-float) (complex-double-float complex-double-float))
-(defop (- complex-long-float--) (complex-long-float) (complex-long-float complex-long-float))
+(defop (- subtract-integers) (integer) (integer integer))
+(defop (- subtract-rationals) (rational) (rational rational))
+(defop (- subtract-short-floats) (short-float) (short-float short-float))
+(defop (- subtract-single-floats) (single-float) (single-float single-float))
+(defop (- subtract-double-floats) (double-float) (double-float double-float))
+(defop (- subtract-long-floats) (long-float) (long-float long-float))
+(defop (- subtract-complex-short-floats) (complex-short-float) (complex-short-float complex-short-float))
+(defop (- subtract-complex-single-floats) (complex-single-float) (complex-single-float complex-single-float))
+(defop (- subtract-complex-double-floats) (complex-double-float) (complex-double-float complex-double-float))
+(defop (- subtract-complex-long-floats) (complex-long-float) (complex-long-float complex-long-float))
 
-(defop (- binary--) (number) (number number) (a b)
+(defop (- subtract) (number) (number number) (a b)
   (type-code-subtypecase (numeric-contagion a b)
     ((not number) (abort-specialization))
     (integer
      (rewrite-as
-      (integer--
+      (subtract-integers
        (the-integer a)
        (the-integer b))))
     (rational
      (rewrite-as
-      (rational--
+      (subtract-rationals
        (the-rational a)
        (the-rational b))))
     (short-float
      (rewrite-as
-      (short-float--
+      (subtract-short-floats
        (coerce-to-short-float a)
        (coerce-to-short-float b))))
     (single-float
      (rewrite-as
-      (single-float--
+      (subtract-single-floats
        (coerce-to-single-float a)
        (coerce-to-single-float b))))
     (double-float
      (rewrite-as
-      (double-float--
+      (subtract-double-floats
        (coerce-to-double-float a)
        (coerce-to-double-float b))))
     (long-float
      (rewrite-as
-      (long-float--
+      (subtract-long-floats
        (coerce-to-long-float a)
        (coerce-to-long-float b))))
     ((complex short-float)
      (rewrite-as
-      (complex-short-float--
+      (subtract-complex-short-floats
        (coerce-to-complex-short-float a)
        (coerce-to-complex-short-float b))))
     ((complex single-float)
      (rewrite-as
-      (complex-single-float--
+      (subtract-complex-single-floats
        (coerce-to-complex-single-float a)
        (coerce-to-complex-single-float b))))
     ((complex double-float)
      (rewrite-as
-      (complex-double-float--
+      (subtract-complex-double-floats
        (coerce-to-complex-double-float a)
        (coerce-to-complex-double-float b))))
     ((complex long-float)
      (rewrite-as
-      (complex-long-float--
+      (subtract-complex-long-floats
        (coerce-to-complex-long-float a)
        (coerce-to-complex-long-float b))))))
 
+(defop (- negate-integer) (integer) (integer))
+(defop (- negate-rational) (rational) (rational))
+(defop (- negate-short-float) (short-float) (short-float))
+(defop (- negate-single-float) (single-float) (single-float))
+(defop (- negate-double-float) (double-float) (double-float))
+(defop (- negate-long-float) (long-float) (long-float))
+(defop (- negate-complex-short-float) (complex-short-float) (complex-short-float))
+(defop (- negate-complex-single-float) (complex-single-float) (complex-single-float))
+(defop (- negate-complex-double-float) (complex-double-float) (complex-double-float))
+(defop (- negate-complex-long-float) (complex-long-float) (complex-long-float))
+(defop (- negate) (number) (number) (x)
+  (type-code-subtypecase x
+    ((not number) (abort-specialization))
+    (integer (rewrite-as (negate-integer x)))
+    (rational (rewrite-as (negate-rational x)))
+    (short-float (rewrite-as (negate-short-float x)))
+    (single-float (rewrite-as (negate-single-float x)))
+    (double-float (rewrite-as (negate-double-float x)))
+    (long-float (rewrite-as (negate-long-float x)))
+    (complex-short-float (rewrite-as (negate-complex-short-float x)))
+    (complex-single-float (rewrite-as (negate-complex-single-float x)))
+    (complex-double-float (rewrite-as (negate-complex-double-float x)))
+    (complex-long-float (rewrite-as (negate-complex-long-float x)))))
+
 (define-external-rewrite-rule - (&rest numbers)
   (if (null numbers)
-      (rewrite-let () 0)
-      (labels ((rewrite-- (number more-numbers)
-                 (if (null more-numbers)
-                     (rewrite-let ((number (process-argument number)))
-                       (rewrite-as (the-number number)))
-                     (rewrite-let ((a (process-argument number))
-                                   (b (rewrite-- (first more-numbers) (rest more-numbers))))
-                       (rewrite-as (binary-- a b))))))
-        (rewrite-- (first numbers) (rest numbers)))))
-
+      (abort-specialization)
+      (multiple-value-bind (type-codes value)
+          (rewrite-let ((number (process-argument (pop numbers))))
+            (if (null numbers)
+                (rewrite-as (negate number))
+                (rewrite-as (the-number number))))
+        (loop while numbers do
+          (multiple-value-setq (type-codes value)
+            (rewrite-let ((a (values type-codes value))
+                          (b (process-argument (pop numbers))))
+              (rewrite-as (subtract a b)))))
+        (values type-codes value))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Multiplication
 
-(defop (* integer-*) (integer) (integer integer))
-(defop (* rational-*) (rational) (rational rational))
-(defop (* short-float-*) (short-float) (short-float short-float))
-(defop (* single-float-*) (single-float) (single-float single-float))
-(defop (* double-float-*) (double-float) (double-float double-float))
-(defop (* long-float-*) (long-float) (long-float long-float))
-(defop (* complex-short-float-*) (complex-short-float) (complex-short-float complex-short-float))
-(defop (* complex-single-float-*) (complex-single-float) (complex-single-float complex-single-float))
-(defop (* complex-double-float-*) (complex-double-float) (complex-double-float complex-double-float))
-(defop (* complex-long-float-*) (complex-long-float) (complex-long-float complex-long-float))
+(defop (* multiply-integers) (integer) (integer integer))
+(defop (* multiply-rationals) (rational) (rational rational))
+(defop (* multiply-short-floats) (short-float) (short-float short-float))
+(defop (* multiply-single-floats) (single-float) (single-float single-float))
+(defop (* multiply-double-floats) (double-float) (double-float double-float))
+(defop (* multiply-long-floats) (long-float) (long-float long-float))
+(defop (* multiply-complex-short-floats) (complex-short-float) (complex-short-float complex-short-float))
+(defop (* multiply-complex-single-floats) (complex-single-float) (complex-single-float complex-single-float))
+(defop (* multiply-complex-double-floats) (complex-double-float) (complex-double-float complex-double-float))
+(defop (* multiply-complex-long-floats) (complex-long-float) (complex-long-float complex-long-float))
 
-(defop (* binary-*) (number) (number number) (a b)
+(defop (* multiply) (number) (number number) (a b)
   (type-code-subtypecase (numeric-contagion a b)
     ((not number) (abort-specialization))
     (integer
      (rewrite-as
-      (integer-*
+      (multiply-integers
        (the-integer a)
        (the-integer b))))
     (rational
      (rewrite-as
-      (rational-*
+      (multiply-rationals
        (the-rational a)
        (the-rational b))))
     (short-float
      (rewrite-as
-      (short-float-*
+      (multiply-short-floats
        (coerce-to-short-float a)
        (coerce-to-short-float b))))
     (single-float
      (rewrite-as
-      (single-float-*
+      (multiply-single-floats
        (coerce-to-single-float a)
        (coerce-to-single-float b))))
     (double-float
      (rewrite-as
-      (double-float-*
+      (multiply-double-floats
        (coerce-to-double-float a)
        (coerce-to-double-float b))))
     (long-float
      (rewrite-as
-      (long-float-*
+      (multiply-long-floats
        (coerce-to-long-float a)
        (coerce-to-long-float b))))
     ((complex short-float)
      (rewrite-as
-      (complex-short-float-*
+      (multiply-complex-short-floats
        (coerce-to-complex-short-float a)
        (coerce-to-complex-short-float b))))
     ((complex single-float)
      (rewrite-as
-      (complex-single-float-*
+      (multiply-complex-single-floats
        (coerce-to-complex-single-float a)
        (coerce-to-complex-single-float b))))
     ((complex double-float)
      (rewrite-as
-      (complex-double-float-*
+      (multiply-complex-double-floats
        (coerce-to-complex-double-float a)
        (coerce-to-complex-double-float b))))
     ((complex long-float)
      (rewrite-as
-      (complex-long-float-*
+      (multiply-complex-long-floats
        (coerce-to-complex-long-float a)
        (coerce-to-complex-long-float b))))))
 
 (define-external-rewrite-rule * (&rest numbers)
   (if (null numbers)
-      (rewrite-let () (rewrite-as 1))
-      (labels ((rewrite-* (number more-numbers)
-                 (if (null more-numbers)
-                     (rewrite-let ((number (process-argument number)))
-                       (rewrite-as (the-number number)))
-                     (rewrite-let ((a (process-argument number))
-                                   (b (rewrite-* (first more-numbers) (rest more-numbers))))
-                       (rewrite-as (binary-* a b))))))
-        (rewrite-* (first numbers) (rest numbers)))))
+      (rewrite-let () 1)
+      (multiple-value-bind (type-codes value)
+          (rewrite-let ((number (process-argument (pop numbers))))
+            (rewrite-as (the-number number)))
+        (loop while numbers do
+          (multiple-value-setq (type-codes value)
+            (rewrite-let ((a (values type-codes value))
+                          (b (process-argument (pop numbers))))
+              (rewrite-as (multiply a b)))))
+        (values type-codes value))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Division
+
+(defop (/ divide-integers) (integer) (integer integer))
+(defop (/ divide-rationals) (rational) (rational rational))
+(defop (/ divide-short-floats) (short-float) (short-float short-float))
+(defop (/ divide-single-floats) (single-float) (single-float single-float))
+(defop (/ divide-double-floats) (double-float) (double-float double-float))
+(defop (/ divide-long-floats) (long-float) (long-float long-float))
+(defop (/ divide-complex-short-floats) (complex-short-float) (complex-short-float complex-short-float))
+(defop (/ divide-complex-single-floats) (complex-single-float) (complex-single-float complex-single-float))
+(defop (/ divide-complex-double-floats) (complex-double-float) (complex-double-float complex-double-float))
+(defop (/ divide-complex-long-floats) (complex-long-float) (complex-long-float complex-long-float))
+
+(defop (/ divide) (number) (number number) (a b)
+  (type-code-subtypecase (numeric-contagion a b)
+    ((not number) (abort-specialization))
+    (integer
+     (rewrite-as
+      (divide-integers
+       (the-integer a)
+       (the-integer b))))
+    (rational
+     (rewrite-as
+      (divide-rationals
+       (the-rational a)
+       (the-rational b))))
+    (short-float
+     (rewrite-as
+      (divide-short-floats
+       (coerce-to-short-float a)
+       (coerce-to-short-float b))))
+    (single-float
+     (rewrite-as
+      (divide-single-floats
+       (coerce-to-single-float a)
+       (coerce-to-single-float b))))
+    (double-float
+     (rewrite-as
+      (divide-double-floats
+       (coerce-to-double-float a)
+       (coerce-to-double-float b))))
+    (long-float
+     (rewrite-as
+      (divide-long-floats
+       (coerce-to-long-float a)
+       (coerce-to-long-float b))))
+    ((complex short-float)
+     (rewrite-as
+      (divide-complex-short-floats
+       (coerce-to-complex-short-float a)
+       (coerce-to-complex-short-float b))))
+    ((complex single-float)
+     (rewrite-as
+      (divide-complex-single-floats
+       (coerce-to-complex-single-float a)
+       (coerce-to-complex-single-float b))))
+    ((complex double-float)
+     (rewrite-as
+      (divide-complex-double-floats
+       (coerce-to-complex-double-float a)
+       (coerce-to-complex-double-float b))))
+    ((complex long-float)
+     (rewrite-as
+      (divide-complex-long-floats
+       (coerce-to-complex-long-float a)
+       (coerce-to-complex-long-float b))))))
+
+(define-external-rewrite-rule / (&rest numbers)
+  (if (null numbers)
+      (rewrite-let () 1)
+      (multiple-value-bind (type-codes value)
+          (rewrite-let ((number (process-argument (pop numbers))))
+            (rewrite-as (the-number number)))
+        (loop while numbers do
+          (multiple-value-setq (type-codes value)
+            (rewrite-let ((a (values type-codes value))
+                          (b (process-argument (pop numbers))))
+              (rewrite-as (divide a b)))))
+        (values type-codes value))))
