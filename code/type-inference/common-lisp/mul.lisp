8d0c797cab7efa093214897f219accb7ccd92e48
Marco Heisig <marco.heisig@fau.de>
2020-01-21 11:04:46 +0100

    Further improve specialization of * and +.

--- code/type-inference/common-lisp/mul.lisp
+++ code/type-inference/common-lisp/mul.lisp
@@ -2,6 +2,10 @@
 
 (in-package #:petalisp.type-inference)
 
+(defun one-ntype-p (ntype)
+  (and (numberp ntype)
+       (= ntype 1)))
+
 (define-specializer * (&rest numbers)
   (trivia:match numbers
     ((list)
@@ -9,56 +13,74 @@
     ((list number)
      (wrap
       (the-number number)))
-    (_
+    (numbers
      (reduce
       (lambda (a b)
-        (ntype-subtypecase
-            (numeric-contagion
-             (wrapper-ntype a)
-             (wrapper-ntype b))
-          ((not number) (abort-specialization))
-          (short-float
-           (wrap
-            (short-float*
-             (coerce-to-short-float a)
-             (coerce-to-short-float b))))
-          (single-float
-           (wrap
-            (single-float*
-             (coerce-to-single-float a)
-             (coerce-to-single-float b))))
-          (double-float
-           (wrap
-            (double-float*
-             (coerce-to-double-float a)
-             (coerce-to-double-float b))))
-          (long-float
-           (wrap
-            (long-float*
-             (coerce-to-long-float a)
-             (coerce-to-long-float b))))
-          ((complex short-float)
-           (wrap
-            (complex-short-float*
-             (coerce-to-complex-short-float a)
-             (coerce-to-complex-short-float b))))
-          ((complex single-float)
-           (wrap
-            (complex-single-float*
-             (coerce-to-complex-single-float a)
-             (coerce-to-complex-single-float b))))
-          ((complex double-float)
-           (wrap
-            (complex-double-float*
-             (coerce-to-complex-double-float a)
-             (coerce-to-complex-double-float b))))
-          ((complex long-float)
-           (wrap
-            (complex-long-float*
-             (coerce-to-complex-long-float a)
-             (coerce-to-complex-long-float b))))
-          (t
-           (wrap-default (ntype 'number)))))
+        (let* ((ntype-of-a (wrapper-ntype a))
+               (ntype-of-b (wrapper-ntype b))
+               (result-ntype (numeric-contagion ntype-of-a ntype-of-b)))
+          (cond
+            ((or (zero-ntype-p ntype-of-a)
+                 (zero-ntype-p ntype-of-b))
+             (funcall (specializer 'coerce)
+                      0
+                      (wrap-constant (type-specifier result-ntype))))
+            ((one-ntype-p ntype-of-a)
+             (funcall (specializer 'coerce)
+                      b
+                      (wrap-constant (type-specifier result-ntype))))
+            ((one-ntype-p ntype-of-b)
+             (funcall (specializer 'coerce)
+                      a
+                      (wrap-constant (type-specifier result-ntype))))
+            (t
+             (ntype-subtypecase
+                 (numeric-contagion
+                  (wrapper-ntype a)
+                  (wrapper-ntype b))
+               ((not number) (abort-specialization))
+               (short-float
+                (wrap
+                 (short-float*
+                  (coerce-to-short-float a)
+                  (coerce-to-short-float b))))
+               (single-float
+                (wrap
+                 (single-float*
+                  (coerce-to-single-float a)
+                  (coerce-to-single-float b))))
+               (double-float
+                (wrap
+                 (double-float*
+                  (coerce-to-double-float a)
+                  (coerce-to-double-float b))))
+               (long-float
+                (wrap
+                 (long-float*
+                  (coerce-to-long-float a)
+                  (coerce-to-long-float b))))
+               ((complex short-float)
+                (wrap
+                 (complex-short-float*
+                  (coerce-to-complex-short-float a)
+                  (coerce-to-complex-short-float b))))
+               ((complex single-float)
+                (wrap
+                 (complex-single-float*
+                  (coerce-to-complex-single-float a)
+                  (coerce-to-complex-single-float b))))
+               ((complex double-float)
+                (wrap
+                 (complex-double-float*
+                  (coerce-to-complex-double-float a)
+                  (coerce-to-complex-double-float b))))
+               ((complex long-float)
+                (wrap
+                 (complex-long-float*
+                  (coerce-to-complex-long-float a)
+                  (coerce-to-complex-long-float b))))
+               (t
+                (wrap-default (ntype 'number))))))))
       numbers))))
 
 (define-differentiator * (&rest numbers) index
