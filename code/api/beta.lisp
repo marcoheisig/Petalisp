1251ccc00076f054684826d21e30785af97a7343
Marco Heisig <marco.heisig@fau.de>
2020-03-26 17:53:05 +0100

    Write a version of β that doesn't use reductions.
    However, we don't use this new version until it has been thoroughly
    tested and shown to be similarly fast as the old version.
    
    The great news is that as soon as the new version is deemed viable, we can
    eliminate reductions from the Petalisp core.  This should simplify the code
    and all backends a lot.

--- code/api/beta.lisp
+++ code/api/beta.lisp
@@ -11,13 +11,41 @@
           ((zerop (shape-rank input-shape))
            (empty-arrays n-values))
           (t
-           (β-aux function n-values input-shape inputs)))))
+           (β-aux function n-values inputs)))))
 
-#+(or)
-(defun β-aux (function n-values shape inputs)
-  (trivia:ematch shape
+(defun β-aux (function n-values inputs)
+  (trivia:ematch (shape (first inputs))
     ((~ _ ~l _)
-     )))
+     (values-list
+      (mapcar (lambda (input) (drop-axes input 0)) inputs)))
+    ((~r range ~l more-ranges)
+     (multiple-value-bind (n rem) (floor (range-size range) 2)
+       (multiple-value-bind (start step end) (range-start-step-end range)
+         (let* ((left (~ start
+                         (* 2 step)
+                         (+ start (* 2 step (1- n)))
+                         ~l more-ranges))
+                (right (~ (+ start step)
+                          (* 2 step)
+                          (+ start (* 2 step n))
+                          ~l more-ranges))
+                (values
+                 (multiple-value-list
+                  (apply #'α* n-values function
+                         (petalisp.utilities:with-collectors ((arguments collect))
+                           (dolist (input inputs)
+                             (collect (reshape input left)))
+                           (dolist (input inputs)
+                             (collect (reshape input right)))
+                           (arguments))))))
+           (if (zerop rem)
+               (β-aux function n-values values)
+               (β-aux function n-values
+                      (let ((rest (~ end ~l more-ranges)))
+                        (loop for input in inputs
+                              for value in values
+                              collect
+                              (stack 0 value (reshape input rest))))))))))))
 
 (defun β* (f z x &optional axis)
   (cond ((empty-array-p x) z)
