d03af40882cb86ac8e07a3a0101d1a8155ed76ab
Marco Heisig <marco.heisig@fau.de>
2020-03-26 17:45:00 +0100

    Improve array stacking, especially with step sizes other than 1.

--- code/api/stack.lisp
+++ code/api/stack.lisp
@@ -3,20 +3,55 @@
 (in-package #:petalisp.api)
 
 (defun stack (axis &rest arrays)
-  (flet ((stacker (axis)
-           (let ((offset 0))
-             (lambda (array)
-               (let* ((lazy-array (lazy-array array))
-                      (shape (shape lazy-array))
-                      (rank (shape-rank shape))
-                      (offsets (make-array rank :initial-element 0)))
-                 (unless (< -1 axis rank)
-                   (error "~@<Invalid stack axis ~S for the array ~S.~:@>"
-                          axis array))
-                 (setf (aref offsets axis) offset)
-                 (prog1 (reshape lazy-array (make-transformation :offsets offsets))
-                   (incf offset
-                         (let ((range (nth axis (shape-ranges shape))))
-                           (- (+ (range-end range) (range-step range))
-                              (range-start range))))))))))
-    (apply #'fuse (mapcar (stacker axis) arrays))))
+  (let ((lazy-arrays (mapcar #'lazy-array arrays))
+        (stack-rank nil)
+        (stack-width nil))
+    ;; Loop over all arrays to validate the input and to determine a
+    ;; suitable stack width.
+    (loop for lazy-array in lazy-arrays and index from 0 do
+      (let* ((shape (shape lazy-array))
+             (rank (shape-rank shape)))
+        ;; Determine the stack rank.
+        (cond ((null stack-rank)
+               (setf stack-rank rank))
+              ((= rank stack-rank)
+               (values))
+              (t
+               (error "~@<Cannot stack arrays with varying ranks.~:@>")))
+        ;; Determine the stack axis.
+        (unless (< -1 axis rank)
+          (error "~@<Invalid stack axis ~S for the array ~S.~:@>"
+                 axis (nth index arrays)))
+        (let ((range (nth axis (shape-ranges shape))))
+          (unless (size-one-range-p range)
+            (cond ((null stack-width)
+                   (setf stack-width (range-size range)))
+                  ((= stack-width (range-size range))
+                   (values))
+                  (t
+                   (error "~@<Cannot stack arrays with varying step sizes.~:@>")))))))
+    ;; If there are no arrays with a size larger than one in the specified
+    ;; axis, we pick a reasonable default.
+    (when (null stack-width)
+      (setf stack-width 1))
+    ;; Now stack the arrays.
+    (apply
+     #'fuse
+     (mapcar
+      (let ((position nil))
+        (lambda (lazy-array)
+          (multiple-value-bind (start step end)
+              (range-start-step-end (nth axis (shape-ranges (shape lazy-array))))
+            (declare (ignore step))
+            (cond ((null position)
+                   (setf position (+ end stack-width))
+                   lazy-array)
+                  ((= position start)
+                   (setf position (+ end stack-width))
+                   lazy-array)
+                  (t
+                   (let ((offsets (make-array stack-rank :initial-element 0)))
+                     (setf (aref offsets axis) (- position start))
+                     (setf position (+ end stack-width))
+                     (reshape lazy-array (make-transformation :offsets offsets))))))))
+      lazy-arrays))))
