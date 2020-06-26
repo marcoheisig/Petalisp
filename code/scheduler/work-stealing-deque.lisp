bd21489abec2632283e6323d689333af54844912
Marco Heisig <marco.heisig@fau.de>
2020-06-26 14:39:44 +0200

    Reorder the work stealing deque code for proper inlining.

--- code/scheduler/work-stealing-deque.lisp
+++ code/scheduler/work-stealing-deque.lisp
@@ -7,6 +7,55 @@
 ;;; The work-stealing deque allows its owner to push and pop elements at
 ;;; the bottom, and it allows other threads to steal elements from the top.
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Circular Array
+
+(defstruct (circular-array
+            (:copier nil)
+            (:predicate circular-array-p)
+            (:constructor make-circular-array
+                (log-size
+                 &aux (segment (make-array (ash 1 log-size))))))
+  (log-size nil :type (integer 0 (#.(integer-length most-positive-fixnum))))
+  (segment nil :type simple-vector))
+
+(declaim (inline circular-array-size))
+(defun circular-array-size (circular-array)
+  (declare (circular-array circular-array))
+  (ash 1 (circular-array-log-size circular-array)))
+
+(declaim (inline circular-array-elt))
+(defun circular-array-elt (circular-array index)
+  (declare (circular-array circular-array))
+  (declare (fixnum index))
+  (svref (circular-array-segment circular-array)
+         (logand index (1- (ash 1 (circular-array-log-size circular-array))))))
+
+(declaim (inline (setf circular-array-elt)))
+(defun (setf circular-array-elt) (value circular-array index)
+  (declare (circular-array circular-array))
+  (declare (fixnum index))
+  (setf (svref (circular-array-segment circular-array)
+               (logand index (1- (ash 1 (circular-array-log-size circular-array)))))
+        value))
+
+(declaim (ftype (function (circular-array fixnum fixnum) (values circular-array &optional))
+                circular-array-grow))
+(defun circular-array-grow (circular-array bottom top)
+  (declare (circular-array circular-array))
+  (declare (fixnum bottom))
+  (declare (fixnum top))
+  (let ((new (make-circular-array (1+ (circular-array-log-size circular-array)))))
+    (loop for index fixnum from top below bottom do
+      (setf (circular-array-elt new index)
+            (circular-array-elt circular-array index)))
+    new))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Work Stealing Deque
+
 (defstruct (work-stealing-deque
             (:copier nil)
             (:constructor make-work-stealing-deque ()))
@@ -70,47 +119,3 @@
               (values object t)
               (work-stealing-deque-steal work-stealing-deque))))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Circular Array
-
-(defstruct (circular-array
-            (:copier nil)
-            (:predicate circular-array-p)
-            (:constructor make-circular-array
-                (log-size
-                 &aux (segment (make-array (ash 1 log-size))))))
-  (log-size nil :type (integer 0 (#.(integer-length most-positive-fixnum))))
-  (segment nil :type simple-vector))
-
-(declaim (inline circular-array-size))
-(defun circular-array-size (circular-array)
-  (declare (circular-array circular-array))
-  (ash 1 (circular-array-log-size circular-array)))
-
-(declaim (inline circular-array-elt))
-(defun circular-array-elt (circular-array index)
-  (declare (circular-array circular-array))
-  (declare (fixnum index))
-  (svref (circular-array-segment circular-array)
-         (logand index (1- (ash 1 (circular-array-log-size circular-array))))))
-
-(declaim (inline (setf circular-array-elt)))
-(defun (setf circular-array-elt) (value circular-array index)
-  (declare (circular-array circular-array))
-  (declare (fixnum index))
-  (setf (svref (circular-array-segment circular-array)
-               (logand index (1- (ash 1 (circular-array-log-size circular-array)))))
-        value))
-
-(declaim (ftype (function (circular-array fixnum fixnum) (values circular-array &optional))
-                circular-array-grow))
-(defun circular-array-grow (circular-array bottom top)
-  (declare (circular-array circular-array))
-  (declare (fixnum bottom))
-  (declare (fixnum top))
-  (let ((new (make-circular-array (1+ (circular-array-log-size circular-array)))))
-    (loop for index fixnum from top below bottom do
-      (setf (circular-array-elt new index)
-            (circular-array-elt circular-array index)))
-    new))
