3df33c0fa5a77034b581dba71f6d3eac33b71053
Marco Heisig <marco.heisig@fau.de>
2020-08-28 10:12:34 +0200

    Add missing file.

---
+++ code/api/indices.lisp
@@ -0,0 +1,18 @@
+;;;; © 2016-2020 Marco Heisig         - license: GNU AGPLv3 -*- coding: utf-8 -*-
+
+(in-package #:petalisp.api)
+
+(defun shape-indices (shape &optional (axis 0))
+  (declare (shape shape))
+  (let ((rank (shape-rank shape)))
+    (unless (<= 0 axis (1- rank))
+      (error "~@<Invalid axis ~A for a shape with rank ~D.~:@>" axis rank))
+    (lazy-reshape
+     (make-range-immediate (nth axis (shape-ranges shape)))
+     shape
+     (make-transformation
+      :input-rank rank
+      :output-mask (vector axis)))))
+
+(defun array-indices (array &optional (axis 0))
+  (shape-indices (array-shape array) axis))
