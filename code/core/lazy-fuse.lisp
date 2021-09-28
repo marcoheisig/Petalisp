3b6cb42933f7f4b2d63b2d48c0d92c19a6b701b8
Marco Heisig <marco.heisig@fau.de>
2021-09-28 11:28:11 +0200

    Optimize fusions that are actually broadcasts.

--- code/core/lazy-fuse.lisp
+++ code/core/lazy-fuse.lisp
@@ -44,21 +44,52 @@
           (error "~@<Cannot fuse the arrays ~
                      ~{~#[~;and ~S~;~S ~:;~S, ~]~}.~:@>"
                  lazy-arrays))
-        ;; If the content of the fusion is predicted to be a
-        ;; constant, we replace the entire fusion by a reference to
-        ;; that constant.  Otherwise, we create a regular lazy-fuse
-        ;; object.
-        (if (petalisp.type-inference:eql-ntype-p ntype)
+        ;; Optimization: If the content of the fusion is predicted to be a
+        ;; constant, we replace the entire fusion by a reference to that
+        ;; constant.
+        (when (petalisp.type-inference:eql-ntype-p ntype)
+          (return-from lazy-fuse
             (lazy-ref
              (lazy-array-from-scalar ntype)
              shape
              (make-transformation
               :input-rank (shape-rank shape)
-              :output-rank 0))
-            (make-lazy-array
-             :delayed-action (make-delayed-fuse :inputs lazy-arrays)
-             :shape shape
-             :ntype ntype
-             :depth (1+ (maxdepth lazy-arrays))))))))
+              :output-rank 0))))
+        ;; Optimization: If the fusion is equivalent to a broadcasting
+        ;; reference (i.e., when all inputs are lazy reshapes of the same
+        ;; lazy array, and all transformations of those reshapes are
+        ;; similar), we replace the entire fusion by such a broadcasting
+        ;; reference.
+        (when (delayed-reshape-p (lazy-array-delayed-action first))
+          (let* ((delayed-reshape (lazy-array-delayed-action first))
+                 (first-input (delayed-reshape-input delayed-reshape))
+                 (first-transformation (delayed-reshape-transformation delayed-reshape))
+                 (first-output-mask (transformation-output-mask first-transformation))
+                 (first-scalings (transformation-scalings first-transformation))
+                 (first-offsets (transformation-offsets first-transformation)))
+            (when (loop for lazy-array in rest
+                        for action = (lazy-array-delayed-action lazy-array)
+                        always
+                        (and (delayed-reshape-p action)
+                             (eq (delayed-reshape-input action) first-input)
+                             (let ((transformation (delayed-reshape-transformation action)))
+                               (and (equalp first-output-mask (transformation-output-mask transformation))
+                                    (equalp first-scalings (transformation-scalings transformation))
+                                    (equalp first-offsets (transformation-offsets transformation))))))
+              (return-from lazy-fuse
+                (lazy-ref
+                 first-input
+                 shape
+                 (make-transformation
+                  :input-rank (transformation-input-rank first-transformation)
+                  :output-mask first-output-mask
+                  :scalings first-scalings
+                  :offsets first-offsets))))))
+        ;; Default: Create a lazy array that performs the specified fusion.
+        (make-lazy-array
+         :delayed-action (make-delayed-fuse :inputs lazy-arrays)
+         :shape shape
+         :ntype ntype
+         :depth (1+ (maxdepth lazy-arrays)))))))
 
 
