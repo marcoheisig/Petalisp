d009657c0410beb8392163bda8975f0f79464572
Marco Heisig <marco.heisig@fau.de>
2022-01-12 15:13:31 +0100

    Correct an in-package form.

--- code/ir/device.lisp
+++ code/ir/device.lisp
@@ -1,6 +1,6 @@
 ;;;; © 2016-2022 Marco Heisig         - license: GNU AGPLv3 -*- coding: utf-8 -*-
 
-(in-package #:petalisp.core)
+(in-package #:petalisp.ir)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
