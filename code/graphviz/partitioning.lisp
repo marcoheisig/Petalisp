a389635ba1ed68e46bc926ddd70fda4d35cfea43
Marco Heisig <marco.heisig@fau.de>
2023-08-09 15:34:27 +0200

    Improve the Graphviz visualization of partitioning graphs.

--- code/graphviz/partitioning.lisp
+++ code/graphviz/partitioning.lisp
@@ -104,9 +104,13 @@
    *kernel-cluster-table*
    (make-instance 'cl-dot:cluster
      :attributes
-     (list :label (format nil "~A" kernel)
+     (list :label `(:html
+                    ()
+                    (:b () "iteration-space:") ,(stringify (petalisp.ir:kernel-iteration-space kernel))
+                    (:br ())
+                    (:b () "number:") ,(stringify (petalisp.ir:kernel-number kernel)))
            :penwidth 2.0
-           :color "gray"))))
+           :bgcolor "gray"))))
 
 (defmethod cl-dot:graph-object-cluster
     ((graph partitioning-graph)
@@ -125,13 +129,14 @@
     :attributes
     `(:label
       (:html () ,(kernel-shard-label node))
+      :fillcolor "#B2DF8A"
       ,@(graphviz-node-attributes graph node))))
 
 (defun kernel-shard-label (kernel-shard)
   (let* ((shape (petalisp.ir:kernel-shard-iteration-space kernel-shard)))
     `(:table
       ((:border "0") (:cellborder "0") (:cellspacing "0") (:cellpadding "0"))
-      (:tr () (:td () ,(format nil "~A" shape))))))
+      (:tr () (:td () ,(stringify shape))))))
 
 (defmethod cl-dot:graph-object-node
     ((graph partitioning-graph)
@@ -146,70 +151,95 @@
   (let* ((split (petalisp.ir:buffer-shard-split buffer-shard))
          (shape (petalisp.ir:buffer-shard-shape buffer-shard))
          (domain (petalisp.ir:buffer-shard-domain buffer-shard))
+         (buffer (petalisp.ir:buffer-shard-buffer buffer-shard))
          (storage (petalisp.ir:buffer-shard-storage buffer-shard))
          (parent (petalisp.ir:buffer-shard-parent buffer-shard)))
     `(:table
       ((:port ,(buffer-shard-port buffer-shard))
-       (:cellborder "0"))
+       (:cellborder "0")
+       (:cellpadding "5")
+       (:cellspacing "0"))
       (:tr () (:td () (:b () "domain:") ,(format nil "~A" domain)))
       (:tr () (:td () (:b () "shape:") ,(format nil "~A" shape)))
+      ,@(when (not parent)
+          `((:tr () (:td () (:b () "type:")
+                         ,(stringify
+                           (typo:ntype-type-specifier
+                            (petalisp.ir:buffer-ntype buffer)))))))
+      ,@(when split
+          `((:tr () (:td () (:b () "split-axis:") ,(stringify (petalisp.ir:split-axis split))))
+            (:tr () (:td () (:b () "split-position:") ,(stringify (petalisp.ir:split-position split))))
+            (:tr () (:td () ,(split-label split)))))
       ,@(when (and storage
                    (or (not parent)
                        (not (petalisp.ir:buffer-shard-storage parent))))
-          `((:tr () (:td () (:b () "strides:") ,(format nil "~A" (petalisp.ir:storage-strides storage))))
-            (:tr () (:td () (:b () "offset:") ,(format nil "~A" (petalisp.ir:storage-offset storage))))
-            (:tr () (:td () (:b () "size:") ,(format nil "~A" (petalisp.ir:storage-size storage))))
-            (:tr () (:td () (:b () "type:") ,(format nil "~A"
-                                                     (typo:ntype-type-specifier
-                                                      (petalisp.ir:storage-ntype storage)))))
-            (:tr
-             ()
-             (:td
-              ()
-              (:table
-               ((:border "1")
-                (:cellspacing "0")
-                (:cellpadding "0")
-                (:cellborder "0")
-                (:fixedsize "TRUE")
-                (:bgcolor "#ABCEF3"))
-               ,@(multiple-value-bind (nrows ncolumns)
-                     (case (petalisp:shape-rank shape)
-                       (0 (values 1 1))
-                       (1 (values (petalisp:range-size (petalisp:shape-range shape 0)) 1))
-                       (otherwise
-                        (values (petalisp:range-size (petalisp:shape-range shape 0))
-                                (petalisp:range-size (petalisp:shape-range shape 1)))))
-                   (loop for row below nrows
-                         collect
-                         `(:tr ()
-                               ,@(loop for column below ncolumns
-                                       collect `(:td ((:width "5") (:height "5"))))))))))))
-      ,@(when split
-          `((:tr
-             ()
-             (:td
-              ()
-              ,(let* ((axis (petalisp.ir:split-axis split))
-                      (left-child (petalisp.ir:split-left-child split))
-                      (right-child (petalisp.ir:split-right-child split)))
-                 `(:table
-                   ((:border "0"))
-                   ,@(case axis
-                       (0
-                        `((:tr () (:td () ,(buffer-shard-label left-child)))
-                          (:tr () (:td () ,(buffer-shard-label right-child)))))
-                       (1
-                        `((:tr ()
-                               (:td () ,(buffer-shard-label left-child))
-                               (:td () ,(buffer-shard-label right-child)))))
-                       (otherwise
-                        `((:tr ()
-                               (:td () ,(buffer-shard-label left-child))
-                               (:td () ,(format nil "axis ~D split" axis))
-                               (:td () ,(buffer-shard-label right-child)))))))))))))))
+          `((:tr () (:td () (:b () "strides:") ,(stringify (petalisp.ir:storage-strides storage))))
+            (:tr () (:td () (:b () "offset:") ,(stringify (petalisp.ir:storage-offset storage))))
+            (:tr () (:td () (:b () "size:") ,(stringify (petalisp.ir:storage-size storage))))
+            (:tr () (:td () ,(storage-label storage domain shape))))))))
 
 (defun buffer-shard-port (buffer-shard)
   (format nil "buffer~Dshard~{~:[R~;L~]~}"
           (petalisp.ir:buffer-number (petalisp.ir:buffer-shard-buffer buffer-shard))
           (petalisp.ir:buffer-shard-path buffer-shard)))
+
+(defun storage-label (storage domain shape)
+  (declare (petalisp.ir:storage storage)
+           (petalisp:shape domain shape))
+  (let* ((rank (petalisp:shape-rank shape))
+         (size (petalisp:shape-size shape))
+         (nrows (if (zerop rank)
+                    1
+                    (petalisp:range-size
+                     (petalisp:shape-range shape 0))))
+         (ncols (/ size nrows))
+         (logrows (log nrows 10))
+         (logcols (log ncols 10))
+         (logdists
+           (loop for offset from 1 to 10
+                 for pos = 0.0 then log
+                 for log = (log offset 10)
+                 collect
+                 (format nil "~D"
+                         (if (zerop log)
+                             30 ;; TODO
+                             (ceiling (* (- log pos) 50)))))))
+    (petalisp.utilities:with-collectors ((rows collect-row))
+      (loop for rowgroup below (max 1 logrows) do
+        (loop for height in logdists repeat (max 1 (* (- logrows rowgroup) 10)) do
+          (petalisp.utilities:with-collectors ((cols collect-col))
+            (loop for colgroup below (max 1 logcols) do
+              (loop for width in logdists repeat (max 1 (* (- logcols colgroup) 10)) do
+                (collect-col
+                 `(:td ((:height ,height) (:width ,width))))))
+            (collect-row
+             `(:tr () ,@(cols))))))
+      `(:table
+        ((:border "0")
+         (:cellpadding "0")
+         (:cellspacing "0")
+         (:cellborder "1")
+         (:color "#556677")
+         (:fixedsize "TRUE")
+         (:bgcolor "#ABCEF3"))
+        ,@(rows)))))
+
+(defun split-label (split)
+  (let* ((axis (petalisp.ir:split-axis split))
+         (left-child (petalisp.ir:split-left-child split))
+         (right-child (petalisp.ir:split-right-child split)))
+    `(:table
+      ((:border "0"))
+      ,@(case axis
+          (0
+           `((:tr () (:td () ,(buffer-shard-label left-child)))
+             (:tr () (:td () ,(buffer-shard-label right-child)))))
+          (1
+           `((:tr ()
+                  (:td () ,(buffer-shard-label left-child))
+                  (:td () ,(buffer-shard-label right-child)))))
+          (otherwise
+           `((:tr ()
+                  (:td () ,(buffer-shard-label left-child))
+                  (:td () ,(format nil "axis ~D split" axis))
+                  (:td () ,(buffer-shard-label right-child)))))))))
