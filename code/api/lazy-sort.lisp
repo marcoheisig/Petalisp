0a66907eaa328ca6da511d04069fe03525283830
Marco Heisig <marco.heisig@fau.de>
2023-09-22 16:34:41 +0200

    Substantially improve the performance of sorting very large arrays.

--- code/api/lazy-sort.lisp
+++ code/api/lazy-sort.lisp
@@ -8,45 +8,50 @@
     ;; The algorithm used here is Batcher's odd-even sort.
     (loop for p = 1 then (ash p 1) while (< p n) do
       (loop for k = p then (ash k -1) while (>= k 1) do
-        (let* ((offset (mod k p))
-               (divisor (ash p 1))
-               (up (make-transformation :offsets (vector k)))
+        (let* ((up (make-transformation :offsets (vector k)))
                (down (invert-transformation up))
-               (updates
-                 (mapcan
-                  (lambda (upper-shape)
-                    (let* ((lower-shape (transform-shape upper-shape down))
-                           (lower-indices (lazy-index-components lower-shape))
-                           (lower-values (lazy-reshape x 1 lower-shape))
-                           (upper-indices (lazy-reshape (lazy-index-components upper-shape) down))
-                           (upper-values (lazy-reshape x 1 upper-shape down)))
-                      (multiple-value-bind (lo hi)
-                          (lazy-multiple-value 2
-                           'typo:cswap
-                           (lazy 'or
-                            (lazy #'/=
-                             (lazy #'floor lower-indices divisor)
-                             (lazy #'floor upper-indices divisor))
-                            (lazy predicate lower-values upper-values))
-                           lower-values
-                           upper-values)
-                        (list lo (lazy-reshape hi up)))))
-                  (if (< k (1+ (floor (- n offset 1) (* 2 k))))
-                      ;; Create shapes with stride 2k.
-                      (loop for i from 0 below k
-                            collect
-                            (~ (+ offset i k) n (* 2 k)))
-                      ;; Create shapes with stride 1.
-                      (loop for j from (+ offset k) by (* 2 k) below n
-                            collect
-                            (~ j (min (+ j k) n)))))))
-          (setf x (apply #'lazy-overwrite x updates)))))
+               (divisor (ash p 1))
+               (j0 (mod k p)))
+          (multiple-value-bind (blocks rest) (floor (- n j0) (* 2 k))
+            (setf x (apply
+                     #'lazy-fuse
+                     (lazy-reshape x 1 (~ 0 j0))
+                     (lazy-reshape x 1
+                      (let ((last (+ j0 (* 2 k blocks))))
+                        (if (<= rest k)
+                            (~ last n)
+                            (~ (+ last (- rest k)) (+ last k)))))
+                     (mapcan
+                      (lambda (upper-shape)
+                        (let* ((lower-shape (transform-shape upper-shape down))
+                               (lower-indices (lazy-index-components lower-shape))
+                               (lower-values (lazy-reshape x 1 lower-shape))
+                               (upper-indices (lazy-reshape (lazy-index-components upper-shape) down))
+                               (upper-values (lazy-reshape x 1 upper-shape down)))
+                          (multiple-value-bind (lo hi)
+                              (lazy-multiple-value 2
+                               'typo:cswap
+                               (lazy 'or
+                                (lazy #'/=
+                                 (lazy #'floor lower-indices divisor)
+                                 (lazy #'floor upper-indices divisor))
+                                (lazy predicate lower-values upper-values))
+                               lower-values
+                               upper-values)
+                            (list lo (lazy-reshape hi up)))))
+                      (if (< k blocks)
+                          (loop for i from 0 below k
+                                collect
+                                (~ (+ j0 i k) n (* 2 k)))
+                          (loop for j from (+ j0 k) by (* 2 k) below n
+                                collect
+                                (~ j (min (+ j k) n)))))))))))
     x))
 
 ;;; I'm leaving a naive implementation of Batcher's algorithm here as a
 ;;; comment, because it is the basis of the parallel implementation above.  In
 ;;; the parallel version, the two inner loops over j and i have been replaced
-;;; by one large invocation of LAZY-OVERWRITE, and the longer of the two loops
+;;; by one large invocation of LAZY-FUSE, and the longer of the two inner loops
 ;;; is used to construct shapes of all the upper halves of each pairwise
 ;;; comparison.
 #+(or)
