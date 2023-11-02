6cf109a9f621960f274eb4c438c1b0007e8e2a04
Marco Heisig <marco.heisig@fau.de>
2023-11-02 15:15:19 +0100

    Improve type inference for arrays with size one.

--- code/core/lazy-array.lisp
+++ code/core/lazy-array.lisp
@@ -287,16 +287,31 @@
 
 (defun lazy-array-from-array (array)
   (declare (array array))
-  (if (zerop (array-rank array))
-      (lazy-array-from-scalar (aref array))
+  (if (= 1 (array-total-size array))
+      (let* ((value (row-major-aref array 0))
+             (scalar (lazy-array-from-scalar value))
+             (rank (array-rank array)))
+        (if (zerop rank)
+            scalar
+            (make-lazy-array
+             :shape (array-shape array)
+             :ntype (lazy-array-ntype scalar)
+             :delayed-action
+             (make-delayed-reshape
+              :input scalar
+              :transformation
+              (make-transformation
+               :input-mask (make-array rank :initial-element 0)
+               :output-rank 0)))))
       (make-lazy-array
        :shape (array-shape array)
        :ntype (typo:array-element-ntype array)
        :delayed-action
        (make-delayed-array (simplify-array array)))))
 
-;;; Turn arrays into simple arrays.
 (defun simplify-array (array)
+  "Returns an array with the same shape and elements as ARRAY, but that is
+guaranteed to be simple."
   (if (typep array 'simple-array)
       array
       (let ((copy (make-array (array-dimensions array)
